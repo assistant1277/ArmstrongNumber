3c327ffb1594ac01c08c40454f61d1a91d19df5d
assistant1277 <shivarajghangale2002@gmail.com>
2024-09-25 14:17:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using ArmstrongNumber.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongNumber
+{
+    internal class Program
+    {      
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number to check if it is Armstrong number ");
+            int inputNumber = int.Parse(Console.ReadLine());
+            Armstrong.CheckAndPrintArmstrong(inputNumber);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ model/Armstrong.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongNumber.model
+{
+    public class Armstrong
+    {
+        public static int CalculatePower(int baseValue, int exponent)
+        {
+            int result = 1;
+            for (int i = 0; i < exponent; i++)
+            {
+                result *= baseValue;
+            }
+            return result;
+        }
+        public static void CheckAndPrintArmstrong(int number)
+        {
+            int numDigits = number.ToString().Length;
+            int sumOfPowers = 0;
+            int tempNumber = number;
+
+            while (tempNumber > 0)
+            {
+                int lastDigit = tempNumber % 10;
+                sumOfPowers += CalculatePower(lastDigit, numDigits);
+                tempNumber /= 10;
+            }
+            string resultMessage = sumOfPowers == number ? $"{number} is Armstrong number" : $"{number} not an Armstrong number";
+
+            Console.WriteLine(resultMessage);
+        }
+    }
+}
